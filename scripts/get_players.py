f42fe36b7ad4ce66cf9e62c2b3d5cdbcbebc0bc1
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-11-24 10:26:54 +0300

    feat: add player data import script

---
+++ scripts/get_players.py
@@ -0,0 +1,40 @@
+from io import StringIO
+from pathlib import Path
+
+import polars as pl
+import requests
+
+script_path = Path(__file__).parent
+data_dir_path = script_path / "../data"
+save_path = data_dir_path / "players.csv"
+
+
+season = "2025-26"
+url = f"https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/refs/heads/master/data/{season}/players_raw.csv"
+
+
+def main():
+    response = requests.get(url)
+    if response.status_code == 200:
+        df = pl.read_csv(StringIO(response.text))
+        columns_to_keep = [
+            "code",
+            "id",
+            "first_name",
+            "second_name",
+            "web_name",
+            "selected_by_percent",
+            "team",
+            "team_code",
+            "status",
+            "news",
+        ]
+        df_filtered = df.select(columns_to_keep)
+        data_dir_path.mkdir(parents=True, exist_ok=True)
+        df_filtered.write_csv(save_path)
+    else:
+        print("Error fetching data")
+
+
+if __name__ == "__main__":
+    main()
